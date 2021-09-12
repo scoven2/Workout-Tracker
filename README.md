f20e751347cfb0f5a07fa08482297a23ef444205
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-09-12 13:33:33 -0500

    got it working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Workout-Tracker
+
+## Description
+
+A workout tracker that allows you to track both cardio and weight training in one convenient location by date, weight, exercise, and duration. 
--- server.js
+++ server.js
@@ -1,28 +1,35 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const router = require("./routes/api-routes.js");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workout", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-    }
+let db = mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/fitnesstrackerDB",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
 );
 
-app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes.js"));
+// routes
+app.use(router);
+require("./routes/html-routes.js")(app);
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
+
+module.exports = db;
